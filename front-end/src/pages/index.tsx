4eb57f01e23e6a87612a5b68da1a3713a7a05c41
dzeko003 <berenismassamba@gmail.com>
2024-05-09 15:01:07 +0200

    first commit

---
+++ front-end/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Seo } from "@/ui/components/seo";
+
+
+export default function Home() {
+  return (
+    <>
+      <Seo title="coders Monkey" description="Description ..."/>
+      <h1 className="text-red-500">Hello word !</h1>
+    </>
+  );
+}
